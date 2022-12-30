95f13815ac1baa04ea563d08c7ce434fa25490db
LeMatthieu <matthieublaise3@gmail.com>
2022-12-30 01:51:31 +0100

    FRONT -- <main> (10% fait)
    Début de la mis en forme de la page d'acceuil avec choix d'une image en bg et différent test pour le responsive.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Manger local</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
 <header>
@@ -41,6 +42,26 @@
   </div>
 </nav>
 </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </body>
+<main>
+<div class="container-fluid p-0"> 
+<div class="bgimage d-flex">
+  <div class="col">
+  <h1 class="text-white fs-1 fw-bold p-3"> Mangez frais et soutenez vos producteurs locaux  </h1>
+  <p class="text-white fs-3"> Commencez par entré une adresse pour voir les points de vente a proximité :   </p>
+  <form class="d-flex bg-secondary p-3 rounded">
+        <input class="form-control me-sm-2" type="search" placeholder="123 rue de l'exemple, Marseille 13000">
+        <button class="btn btn-success my-2 my-sm-0" type="submit">Rechercher</button>
+      </form>
+  </div>
+  <div class="col">
+     <h1 class="text-white fs-1 fw-bold p-3"> Mangez frais et soutenez vos producteurs locaux  </h1>
+  <p class="text-white fs-3"> Commencez par entré une adresse pour voir les points de vente a proximité :   </p>
+    </div>
+
+</div>
+</div>
+
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
 </html>
